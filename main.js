e1bb888fffe4c45b9301c070ac7cc9bd7dc41e28
dwale <superdwale@gmail.com>
2022-02-02 04:04:49 +0100

    compare emitters that loop over a single key and loops all objects with key

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-var Benchmark = require("benchmark");
-var suite = new Benchmark.Suite();
+const Benchmark = require("benchmark");
+const suite = new Benchmark.Suite();
+const EventEmitterArray = require("./lib/event-emitter").eventEmitterArray;
+const EventEmitterMap = require("./lib/event-emitter-map").eventEmitter;
+
+const eventsArray = new EventEmitterArray();
+const eventsMap = new EventEmitterMap();
+
+const noop = () => {};
+
+for (let i = 0; i < 1000; i++) {
+	eventsArray.listen("foo", noop);
+	eventsMap.listen("foo", noop);
+}
 
 // add tests
 suite
-	.add("RegExp#test", function () {
-		/o/.test("Hello World!");
+	.add("EventEmitterArray", function () {
+		eventsMap.emit("foo");
 	})
-	.add("String#indexOf", function () {
-		"Hello World!".indexOf("o") > -1;
+	.add("EventEmitterMap", function () {
+		eventsArray.emit("foo");
 	})
-	.add("String#match", function () {
-		!!"Hello World!".match(/o/);
-	})
+
 	// add listeners
 	.on("cycle", function (event) {
 		console.log(String(event.target));
